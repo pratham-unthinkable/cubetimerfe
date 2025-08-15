ef602b653f68867f8811eb4811e480b278257d5a
pratham singh <pratham.singh@unthinkable.co>
2025-08-15 23:47:13 +0530

    fixed window

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -1,7 +1,5 @@
 import { apiClient } from "../utils/fetch"
 
-const { token } = JSON.parse(window.localStorage.getItem('token'));
-
 export const login = async ({ username, password }) => {
     return apiClient('https://cubetimerbackend.onrender.com/api/v1/user/auth/login', { method: "POST", data: { username, password } })
 }
